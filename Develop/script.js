f42d6aaca28aabe41127124b52dc56d408c40bb0
cbeisel <collin.beisel@toyota.com>
2021-11-04 14:59:59 -0500

    Completed core functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,129 @@
 // Assignment code here
+//Global arrays and variables
 
+const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+const specialChar = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+
+var confirmLowerCase;
+var comfirmUpperCase;
+var confirmNumbers;
+var confirmSpecialChar;
+
+var passwordLength;
+var passwordString = [];
+var finalPassword = "";
+
+//generate password function
+function generatePassword() {
+  //when user clicks generate password, ask them what the length should be 8-128 and validate their input
+  passwordLength = prompt("Please input the desired length of your password from 8 to 128 characters.");
+ 
+
+  //Loop that will run through the confirm length prompt until correct length range is chosen and the user has entered a valid number
+  while (passwordLength < 8 || passwordLength > 128 || passwordLength == null || isNaN(passwordLength)) {
+    alert("You must choose a length between 8-128. This field only accepts numbers.")
+    passwordLength = prompt("Please input the desired length of your password from 8 to 128 characters.");
+  }
+
+  //print the chosen password length to console to check if it worked
+  console.log("Your password will be " + passwordLength + " characters");
+
+    //confirm if user wants to include lower case letters
+    confirmLowerCase = confirm("Would you like to include LOWER CASE LETTERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmLowerCase) {
+      console.log("You have chosen to include LOWER CASE LETTERS.");
+    }
+    else {
+      console.log("You have chosen not to include LOWER CASE LETTERS.");
+    }
+    //confirm if user wants to include upper case letters
+    confirmUpperCase = confirm("Would you like to include UPPER CASE LETTERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmUpperCase) {
+      console.log("You have chosen to include UPPER CASE LETTERS");
+    }
+    else {
+      console.log("You have chosen not to include UPPER CASE LETTERS");
+    }
+    //confirm if user wants to include numbers
+    confirmNumbers = confirm("Would you like to include NUMBERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmNumbers) {
+      console.log("You have chosen to include special NUMBERS");
+    }
+    else {
+      console.log("You have chosen not to include special NUMBERS");
+    }
+    //confirm if user wants to include special Characters
+    confirmSpecialChar = confirm("Would you like to include SPECIAL CHARACTERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmSpecialChar) {
+      console.log("You have chosen to include special SPECIAL CHARACTERS");
+    }
+    else {
+      console.log("You have chosen not to include special SPECIAL CHARACTERS");
+    }
+
+  //Loop that will run through configuration options until they choose at least 1 parameter
+  while (confirmLowerCase == false && confirmUpperCase == false && confirmNumbers == false && confirmSpecialChar == false) {
+   //Alert user that they must pick at least 1 parameter
+    alert("Please select 1 ore more password parameters, it is recommended to include all for security.")
+    //confirm if user wants to include lower case letters
+    confirmLowerCase = confirm("Would you like to include LOWER CASE LETTERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmLowerCase) {
+      console.log("You have chosen to include LOWER CASE LETTERS.");
+    }
+    else {
+      console.log("You have chosen not to include LOWER CASE LETTERS.");
+    }
+    //confirm if user wants to include upper case letters
+    confirmUpperCase = confirm("Would you like to include UPPER CASE LETTERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmUpperCase) {
+      console.log("You have chosen to include UPPER CASE LETTERS");
+    }
+    else {
+      console.log("You have chosen not to include UPPER CASE LETTERS");
+    }
+    //confirm if user wants to include numbers
+    confirmNumbers = confirm("Would you like to include NUMBERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmNumbers) {
+      console.log("You have chosen to include special NUMBERS");
+    }
+    else {
+      console.log("You have chosen not to include special NUMBERS");
+    }
+    //confirm if user wants to include special Characters
+    confirmSpecialChar = confirm("Would you like to include SPECIAL CHARACTERS in your password? (Click okay to include or cancel to skip)");
+    if (confirmSpecialChar) {
+      console.log("You have chosen to include special SPECIAL CHARACTERS");
+    }
+    else {
+      console.log("You have chosen not to include special SPECIAL CHARACTERS");
+    }
+  }
+  
+  // Generate new array based on criteria user selects... add together contents of each char array and and send the total to a new variable passwordString
+  if(confirmLowerCase) {
+    passwordString = passwordString.concat(lowerCase);
+  }
+  if (confirmUpperCase) {
+    passwordString = passwordString.concat(upperCase);
+  }
+  if (confirmNumbers) {
+    passwordString = passwordString.concat(numbers);
+  }
+  if (confirmSpecialChar) {
+    passwordString = passwordString.concat(specialChar);
+  }
+  console.log(passwordString);
+
+  //loop to pull a random character from the new concatinated array. Will run loop for the length of the new array
+  for (let i = 0; i < passwordLength; i++) {
+    finalPassword = finalPassword + passwordString[Math.floor(Math.random() * passwordString.length)];
+  }
+  // return password to textarea
+  return finalPassword;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +139,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Password Generator Starter Code
+
+## Acceptance Criteria
+GIVEN I need a new, secure password
+### Prompt
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+### Choose Criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+### Enter Length
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+### Choose Char Types
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+### Validate Selections
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+### Generate Password
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+### Display Password
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
